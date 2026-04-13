74428e75662bf819296988e5b6c1fee208533c4f
theekshana-git <theekshana.professional@gmail.com>
2026-04-13 17:04:28 +0530

    feat: scaffold role-based MVC architecture and implement dynamic UI state locks

---
+++ PAS.BlindMatch/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PAS.BlindMatch.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PAS.BlindMatch/Controllers/SupervisorController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PAS.BlindMatch.Controllers
+{
+    [Authorize(Roles = "Supervisor")]
+    public class SupervisorController : Controller
+    {
+        public IActionResult BlindReview()
+        {
+            return View();
+        }
+
+        public IActionResult Expertise()
+        {
+            return View();
+        }
+
+        public IActionResult MyMatches()
+        {
+            return View();
+        }
+    }
+}
